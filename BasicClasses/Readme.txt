c50e1baa36dad9d740116008caba703fd557757e
sicnuthinkgeo <972656027@qq.com>
2016-12-01 23:52:33 +0800

    Three ways to print all items of a List which implements the IEnumerator<int> : IEnumerator.

---
+++ BasicClasses/Readme.txt
@@ -0,0 +1,10 @@
+﻿# Basic Classes
+---
+
+# System.Collections.Generic
+
+1. IEnumerator<> | List
+
+
+
+# FAQ:
---
+++ BasicClasses/ThreeWaysToForeachListObject.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicClasses
+{
+    class ThreeWaysToForeachListObject
+    {
+        public ThreeWaysToForeachListObject()
+        {
+            IList<int> list = new List<int>() { 100,200,300};
+
+            for (int i = 0; i < list.Count(); i++)
+            {
+                Console.WriteLine(list[i]);
+            }
+
+            foreach (var item in list)
+            {
+                Console.WriteLine(item);
+            }
+
+            IEnumerator<int> ienumerator = list.GetEnumerator();
+            while (ienumerator.MoveNext())
+            {
+                Console.WriteLine(ienumerator.Current);
+            }
+        }
+    }
+}
