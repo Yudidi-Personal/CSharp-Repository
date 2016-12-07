0ce32272b65bcf753c43759832fe09501e9f62ff
sicnuthinkgeo <mikeyu@thinkgeo.com>
2016-12-08 01:14:15 +0800

    Add Xml Operations.

--- Readme.txt
+++ Readme.txt
@@ -1,2 +1,28 @@
-﻿#
+﻿
+
+# Some method
+1. XContainer.Descendants
+
+# Work with NameSpace
+
+
+
+
+# Functions
 ---
+
+# 1. Create xml according to two template XML files -- 用于格式固定的xml的一次生成
+
+# 2.
+
+
+# References
+0. base 
+http://www.cnblogs.com/a1656344531/archive/2012/11/28/2792863.html
+
+1. C#操作XML的完整例子——XmlDocument篇
+http://blog.csdn.net/cds27/article/details/2305166
+
+
+2. LINQ to XML 编程基础
+http://www.cnblogs.com/luckdv/articles/1728088.html
---
+++ XML/Functions/CreateXml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Configuration;
+using System.Xml.Linq;
+using System.IO;
+using System.Diagnostics;
+
+namespace XML
+{
+    public class CreateXml
+    {
+        public static string xmlTemplate = ConfigurationManager.AppSettings["XmlTemplate"];
+        public static string newNodeTemplate = ConfigurationManager.AppSettings["NewNodeTemplate"];
+
+        /// <summary>
+        /// Create xml by 2 template files.
+        /// 1. if the new nodes to be added are certain.
+        /// </summary>
+        /// <param name="xmlPath"></param>
+        public static void CreateXmlWithTemplate(string xmlPath)
+        {
+            XDocument xDoc = XDocument.Load(xmlTemplate);
+            XDocument itemDoc = XDocument.Load(newNodeTemplate);
+            // replace 
+            XElement titleElement = itemDoc.Root.Descendants("title").First();
+        }
+
+        public static void CreateXmlByTree()
+        {
+            XElement xmlTree = new XElement("Root",
+               new XAttribute("Att1", "AttributeContent"),
+               new XElement("Child",
+                   new XText("Some text"),
+                   new XElement("GrandChild", "element content")
+               )
+            );
+            XDocument xDoc = new XDocument(xmlTree);
+            xDoc.Save("tmp.xml");
+            Process.Start("tmp.xml");
+            //XElement xmlTree2 = new XElement("Root", new XAttribute("attr", "AttrbuteContent"),
+            //    new XElement("SubNode",) );
+        }
+
+    }
+}
