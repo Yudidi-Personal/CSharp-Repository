3c47830bd394878054beada60b357695e9700f67
Mike-ThinkGeo <mikeyu@thinkgeo.com>
2016-12-19 22:57:13 +0800

    1.泛型 复习.

---
+++ CSharp/LinQ/PreliminaryKnowledge/SystemGenericDelegate/MyGeneric.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SystemGenericDelegate
+{
+    public class MyGeneric<MyGene>
+    {
+        private MyGene GetBiggerOne(MyGene x,MyGene y,bool want)
+        {
+            if (want)
+            {
+                return x;
+            }
+            return default(MyGene);
+        }
+    }
+}
